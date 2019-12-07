b9d2686c3f20bf3829a1f7b0feba8eb511dd0ed8
ziangqin_gcp_deeplearning-base <ziangqin.developer@gmail.com>
2019-12-07 06:03:53 +0000

    first commit after report, from gcp vm

--- debug.py
+++ debug.py
@@ -14,12 +14,13 @@ from lib.config import *
 # DataPreparation.dataset2midi_folder(DATABASE_PATH, TRIO_DATASET_NAME, "./trio_midi")
 
 
-# dataset = DataPreparation.build_dataset(DATA_POOL_PATH, DATABASE_PATH, cutoff=150)
-# trio_dataset = DataPreparation.generate_trio_data(DATABASE_PATH)
-# DataPreparation.dataset2midi_folder(DATABASE_PATH, TRIO_DATASET_NAME, "./trio_midi", trio_dataset)
+# dataset = DataPreparation.build_dataset(DATA_POOL_PATH, DATABASE_PATH, cutoff=150
+trio_dataset = DataPreparation.generate_trio_data(DATABASE_PATH)
+DataPreparation.dataset2midi_folder(DATABASE_PATH, TRIO_DATASET_NAME, "./trio_midi", trio_dataset)
 
-
+ 
 # DataPreparation.build_dataset(DATA_POOL_PATH, DATABASE_PATH, cutoff=0)
 # trio_dataset = lib.data.DataPreparation.generate_trio_data(DATABASE_PATH)
-lib.data.DataPreparation.dataset2midi_folder(DATABASE_PATH, TRIO_DATASET_NAME, "./source/trio_midi")
+
+# lib.data.DataPreparation.dataset2midi_folder(DATABASE_PATH, TRIO_DATASET_NAME, "./source/trio_midi")
 # lib.data.DataPreparation.build_note_sequence_dataset()
